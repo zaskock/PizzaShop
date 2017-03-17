853c86d74cafcb1fdc68aac5984e2cfd7eb55185
Valera <bomzadua@yomail.info>
2017-03-17 02:29:42 +0200

    unnecessary code removed, usefull comments added

--- public/app.js
+++ public/app.js
@@ -1,32 +1,15 @@
-function cart_contains()
-{
-	var x="";
-	for (var i = 0; i < localStorage.length; i++)
-	{
-		//$('body').append(localStorage.getItem(localStorage.key(i)));
-		x= x + (localStorage.key(i)) + " - " + localStorage.getItem(localStorage.key(i)) + "\n";
-	}
-return (x);
-}
-
+//using product id (taken from button) as a key, increase the corresponding value by 1
 function add_to_cart(id)
 {
-var key= 'product_' + id;
-var x=window.localStorage.getItem(key)
-x=x*1+1;
-window.localStorage.setItem(key, x);
-//alert('Added to shopping-cart, product_Id: ' + id + '.\nTotal:\n' + cart_contains());
-update_orders();
-update_button();
+	var key= 'product_' + id;
+	var x=window.localStorage.getItem(key)
+	x=x*1+1;
+	window.localStorage.setItem(key, x);
+	update_orders();
+	update_button();
 }
 
-function update_orders()
-{
-	var orders = get_orders();
-	$('#orders_input').val(orders);
-}
-
-
+//get order data from localstorage and out into string like "product_1=2,...product_n=x,"
 function get_orders()
 {
 	var x="";
@@ -36,7 +19,7 @@ function get_orders()
 	}
 return (x);
 }
-
+//count quantity of products chosen
 function get_orders_number()
 {
 	var x=0;
@@ -46,13 +29,19 @@ function get_orders_number()
 	}
 return (x);
 }
-
+//put order details into field of the form
+function update_orders()
+{
+	var orders = get_orders();
+	$('#orders_input').val(orders);
+}
+//put number of orders into text of a shopping-cart button
 function update_button()
 {
 	var str="Shopping cart (" + get_orders_number() + ")"
 	$('#orders_button').val(str);
 }
-
+//clear shopping-cart (called on order submit)
 function clear_cart()
 {
 	localStorage.clear();
